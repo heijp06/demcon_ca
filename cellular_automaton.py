882d3f35ea1aabe7498574472de146c78412fac4
Peter van der Heijden <peter@vanderheijden.name>
2022-03-13 12:04:59 +0100

    Simplify

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -2,19 +2,20 @@ class CA:
     def __init__(self, cells: list[bool], rule: int) -> None:
         self.cells = cells
         self.rule = rule
-        self.left_edge = False
-        self.right_edge = False
+        self.value_at_infinity = False
 
     def next_generation(self) -> None:
-        extended = [self.left_edge] + self.cells + [self.right_edge]
+        extended = [
+            self.value_at_infinity, *self.cells, self.value_at_infinity
+        ]
         self.cells = [
-            self.next_cell(previous)
-            for previous
+            self.next_cell(left, cell, right)
+            for left, cell, right
             in zip(extended, extended[1:], extended[2:])
         ]
-        self.left_edge = self.next_cell(tuple([self.left_edge] * 3))
-        self.right_edge = self.next_cell(tuple([self.right_edge] * 3))
+        self.value_at_infinity = self.next_cell(
+            self.value_at_infinity, self.value_at_infinity, self.value_at_infinity
+        )
 
-    def next_cell(self, previous: tuple[bool, ...]) -> bool:
-        left, cell, right = previous
+    def next_cell(self, left: bool, cell: bool, right: bool) -> bool:
         return self.rule & (1 << left * 4 + cell * 2 + right) != 0
